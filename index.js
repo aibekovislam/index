86d5bcd0f0e5a824e8dd4b82d3523dd6107cc89e
Islam <78363252+aibekovislam@users.noreply.github.com>
2021-04-26 10:55:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// var num = 23;
+// var num2 = 33;
+
+// console.log("Вычитание: " + (num - num2));
+// console.log("Сложение: " + (num + num2));
+
+// var num3 = 7;
+
+// num3 += 7;
+// console.log(num3)
+
+// var number = 13;
+
+
+// if (number > 14) {
+//     console.log("Правильно")
+// } else if (number == 13) {
+//     console.log("Это другое число!")
+// } else {
+//     console.log("Неверно!");
+// }
+
+// let human = "Владимир";
+// let age = 16;
+// let Home = false;
+// let isMarried = false;
+
+// if (isMarried == true && age > 17) {
+//     console.log("Ты женат")
+//     alert("Поздравляю");
+// } else if (isMarried == false && age < 17) {
+//     console.log(human + " " + "ты к сожелению не женат!");
+// } if (Home == true && isMarried == false) {
+//     alert("Ты не женат но у тебя есть дом: ")
+//     prompt("Опиши твой дом: ");
+// }
+
+
+
+
+// for (let num = 0; num < 6; num++) {
+//     console.log(num);
+// }
